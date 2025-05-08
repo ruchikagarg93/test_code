9d17a63599c6f3fc2877f2a5a2cc047bcd9096b7
Ruchika Garg <ruchikagarg93@gmail.com>
2025-05-08 18:06:26 +0530

    request.py
    Updated code

--- request.py
+++ request.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel, PlainSerializer
 from typing import Annotated, Optional, Union
+from pathlib import Path
 
-from cis.runtime.core.cis_request import CisRequest
-from cis.runtime.core.cis_request import CisAsset
+class CisAsset(BaseModel):
+    """An asset in the request."""
+    
+    name: str
+    path: Union[str, Path]  # This can be a string or Path
+    delimiter: Optional[str] = None  # Optional delimiter for CSV files
+    iso_week: Optional[int] = None  # ISO week (optional)
 
 class CisRequestInput(BaseModel):
     """The input data of the request."""
@@ -13,7 +19,8 @@ class CisRequestInput(BaseModel):
     characteristics: list[str]
     assets: Optional[list[CisAsset]] = None
 
+
 class CisRequestUp(CisRequest):
     """A request received in CIS."""
 
-    input: CisRequestInput 
+    input: CisRequestInput
