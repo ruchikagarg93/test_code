e727f7247381296cf6af228257e82106e0354c2a
Ruchika Garg <ruchikagarg93@gmail.com>
2025-05-14 01:31:16 +0530

    Create mock_worker_test.py

---
+++ mock_worker_test.py
@@ -0,0 +1,25 @@
+from cis.runtime.core import CisRequestInput
+
+# Sample request input
+request_data = {
+    "application": "projectrun",
+    "consumer": "promoflyers",
+    "country": "us",
+    "characteristics": [
+        "promoflyers-metrics"
+    ],
+    "client": "GLT",
+    "input": {
+        "assets": [
+            {
+                "name": "test_name",
+                "path":"/projectrun/promoflyers/input/REQ-db0b6564-fe43-408b-9c9c-9391d1d64a4e/metic_example.csv",
+                "delimiter": ",",
+                "iso_week": 202501
+            }
+        ]
+    }
+}
+
+# Initialize CisRequestInput with the data
+request_input = CisRequestInput(request_data)
