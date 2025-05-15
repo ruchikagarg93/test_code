884a33e7e1c9b75c83429dbc0f5a444a58a7be47
Ruchika Garg <ruchikagarg93@gmail.com>
2025-05-16 02:35:33 +0530

    Update mock_worker_test.py

--- mock_worker_test.py
+++ mock_worker_test.py
@@ -45,37 +45,37 @@ def main():
 
     # 3) Instantiate worker
     worker_instance = Worker(
-    # Azure Blob Storage
-    promoflyer_storage_account=cfg.storage['promoflyer_storage_account'],
-    promoflyer_container_name=cfg.storage['promoflyer_container_name'],
-    token_cis=cfg.storage['token_cis'],
+    # -- Azure Blob Storage
+    promoflyer_storage_account=cfg.storage.promoflyer_storage_account,
+    promoflyer_container_name=cfg.storage.promoflyer_container_name,
+    token_cis=cfg.storage.token_cis,
 
-    # Redis cache
-    redis_cache_host=cfg.redis['cache_host'],
-    redis_cache_port=cfg.redis['cache_port'],
-    redis_cache_password=cfg.redis['cache_password'],
+    # -- Redis cache
+    redis_cache_host=cfg.redis.cache_host,
+    redis_cache_port=cfg.redis.cache_port,
+    redis_cache_password=cfg.redis.cache_password,
 
-    # Redis queue
-    redis_queue_host=cfg.redis['queue_host'],
-    redis_queue_port=cfg.redis['queue_port'],
-    redis_queue_password=cfg.redis['queue_password'],
+    # -- Redis queue
+    redis_queue_host=cfg.redis.queue_host,
+    redis_queue_port=cfg.redis.queue_port,
+    redis_queue_password=cfg.redis.queue_password,
 
-    # Database
-    db_server=cfg.database['server'],
-    db_port=cfg.database['port'],
-    db_name=cfg.database['name'],
-    db_user=cfg.database['user'],
-    db_pass=cfg.database['password'],
+    # -- Database
+    db_server=cfg.database.server,
+    db_port=cfg.database.port,
+    db_name=cfg.database.name,
+    db_user=cfg.database.user,
+    db_pass=cfg.database.password,
 
-    # Azure ML
-    azureml_subscription_id=cfg.azureml['subscription_id'],
-    azureml_resource_group=cfg.azureml['resource_group'],
-    azureml_workspace_name=cfg.azureml['workspace_name'],
-    azureml_tenant_id=cfg.azureml['tenant_id'],
-    azureml_client_id=cfg.azureml['client_id'],
-    azureml_client_secret=cfg.azureml['client_secret']
+    # -- Azure ML
+    azureml_subscription_id=cfg.azureml.subscription_id,
+    azureml_resource_group=cfg.azureml.resource_group,
+    azureml_workspace_name=cfg.azureml.workspace_name,
+    azureml_tenant_id=cfg.azureml.tenant_id,
+    azureml_client_id=cfg.azureml.client_id,
+    azureml_client_secret=cfg.azureml.client_secret,
     )
-
+    
     worker_instance.run(req, output_path, feedback_container)
     print(f"Test run complete. Output CSV at: {output_path}")
 
